e66bb299fb12e4c583a5e62a319e8ec30a6141fd
Gustavo Ferreira <57595826+BillyCoding@users.noreply.github.com>
2020-12-01 16:32:48 -0300

    Feat: Unique Email

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '10m' },
+      signOptions: { expiresIn: '60m' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
