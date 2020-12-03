0dfbc44c22375b42f67f309fa59b42fb39433a04
Gustavo Ferreira <57595826+BillyCoding@users.noreply.github.com>
2020-12-03 10:48:43 -0300

    Feat: Install express-session

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Session } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ResponseDTO } from './DTO/response.dto';
 
@@ -9,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): ResponseDTO {
     return this.appService.getHello();
+  }
+
+  @Get('session')
+  findAll(@Session() session: Record<string, any>) {
+    session.visits = session.visits ? session.visits + 1 : 1;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,19 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { ValidationPipe } from './pipes/validation.pipe';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+  );
+  app.use(
+    session({
+      secret: 'ARROZCOMFEIJAO',
+      resave: false,
+      saveUninitialized: false,
+    }),
   );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000, '0.0.0.0');
